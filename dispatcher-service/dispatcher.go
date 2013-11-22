560a77e1126d0ba08ffb7fe6ef68d0d6c1436cd1
Ted Gould <ted@gould.cx>
2013-11-22 16:49:07 -0600

    Kinda have a connection to DBus

--- dispatcher-service/dispatcher.go
+++ dispatcher-service/dispatcher.go
@@ -1,7 +1,22 @@
 package main
 
+import "launchpad.net/~jamesh/go-dbus/trunk"
 import "fmt"
+import "log"
 
 func main () {
+	var (
+		err error
+		conn *dbus.Connection
+	)
+
+	// Connect to Session or System buses.
+	if conn, err = dbus.Connect(dbus.SessionBus); err != nil {
+		log.Fatal("Connection error:", err)
+	}
+	if err = conn.Authenticate(); err != nil {
+		log.Fatal("Authentication error:", err)
+	}
+
 	fmt.Println("Hello World")
 }
